86c315755798d4bce5add50daf8b34a4274bbe0b
cantabilano <didwnsgus@gmail.com>
2023-03-26 20:35:44 +0900

    Initial Commit

---
+++ lecture03-practice/lecture03_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  lecture03_practiceApp.swift
+//  lecture03-practice
+//
+//  Created by Jason Yang on 2023/03/26.
+//
+
+import SwiftUI
+
+@main
+struct lecture03_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
